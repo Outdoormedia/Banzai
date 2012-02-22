342413620bd7bcc80541f8b48c3255b42f8e5b6e
jwmount <john@venuesoftware.com>
2012-02-22 11:42:21 +1000

    Layout all done except for message texts in footer.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,9 @@ class HomeController < ApplicationController
           @login_message = "Please login: "
           @welcome_message = "Welcome to the MOVE System."
           @login_status_message = "You are not logged in at this time."
+          
+          @copyright_message = "Copyright 2007-12 MOVE Pty Ltd"
+          @organization_name = 'Outdoor Media Association'
+
         end
 end
