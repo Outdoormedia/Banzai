f8ba8ca6aed10e19f3366a070b6180160868012b
jwmount <john@venuesoftware.com>
2012-04-01 11:43:12 +1000

    Equal slices based on 1 per slice, normalize.css added per The Rails View.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+// added modernizr per 'The Rails View', must be first one.
+//= require modernizr-1.7.custom
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
