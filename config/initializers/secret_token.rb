04706b9fc300f2c94107a2ac07deadb5e4dd1fc9
Nigel Rausch <nigelr@brisbanerails.com>
2012-04-18 23:16:44 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Banzai::Application.config.secret_token = 'd3437167c2354e866875de82502972bf64ce73a970baa28bda6dbbe6a57de56dc0b9ef251f08c2166520b3ad1efb2b4176fffb84aa0acb46d42eda6e4f3f98a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Banzai::Application.config.session_store :cookie_store, key: '_banzai_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Banzai::Application.config.session_store :active_record_store
