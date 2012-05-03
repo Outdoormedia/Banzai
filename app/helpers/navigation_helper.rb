221365ba1f3266a00f60d50267ab0ab57f446982
John Mount <john@venuesoftware.com>
2012-05-03 10:00:15 +1000

    Touch up in navigation_helper.rb - removed comments.

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -3,9 +3,7 @@ module NavigationHelper
   # called by layouts/_main_name to create links that are page-aware.
   # In HAML, hence: - nav_tab( 'Homes', root_url, content: current_tab )
   def nav_tab(title, options={})
-#    current_tab = options.delete(title)
     options[:class] = (@page.slug).casecmp(title) == 0 ? 'active' : 'inactive'
-#    logger.info("****** #{@page.slug}, #{title}, #{current_tab}, #{options[:class].to_s} *****")
     content_tag(:li, link_to(title, title), options )    
   end
 
