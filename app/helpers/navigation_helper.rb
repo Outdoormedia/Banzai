b1d55254ff77df604ad9341c137661c4c0b3b7e6
John Mount <john@venuesoftware.com>
2012-05-03 16:15:02 +1000

    Multi-part menu item names operate as for single word names.

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,10 +2,17 @@ module NavigationHelper
 
   # called by layouts/_main_name to create links that are page-aware.
   # In HAML, hence: - nav_tab( 'Homes', root_url, content: current_tab )
-  def nav_tab(title, options={})
+  def xnav_tab(title, options={})
     options[:class] = (@page.slug).casecmp(title) == 0 ? 'active' : 'inactive'
     content_tag(:li, link_to(title, title), options )    
   end
+
+  def nav_tab(title, url=title, options={})
+    logger.info("***** #{@page.slug}, #{url}")
+    options[:class] = (@page.slug).casecmp(url) == 0 ? 'active' : 'inactive'
+    content_tag(:li, link_to(title, url), options )    
+  end
+
 
   def currently_at(tab)
     render partial: 'layouts/main_nav', locals: { current_tab: tab}
