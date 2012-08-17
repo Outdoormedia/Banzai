64e72b17cbaa351f2e6834d402c11df0bdb8989c
Rob Yurkowski <rob@yurkowski.net>
2012-08-16 21:06:00 -0400

    Allow tagging of multiple resources on upload.

--- app/decorators/models/refinery/resource_decorator.rb
+++ app/decorators/models/refinery/resource_decorator.rb
@@ -3,4 +3,20 @@ require 'acts-as-taggable-on'
 Refinery::Resource.class_eval do
   acts_as_taggable
   attr_accessible :tag_list
+
+  class << self
+    def create_resources(params)
+      resources = []
+
+      unless params.present? and params[:file].is_a?(Array)
+        resources << create(params)
+      else
+        params[:file].each do |resource|
+          resources << create({:file => resource}.merge(params.except(:file)))
+        end
+      end
+
+      resources
+    end
+  end
 end
