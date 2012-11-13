ef4bf3b2339a1102eac8b8c478efbcf82abbba30
John Mount <john@venuesoftware.com>
2012-11-13 13:07:27 +1000

    'About OMA' and in fact all :http links are target: _blank per request of JG.

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,6 +2,7 @@ module NavigationHelper
 
   # called by layouts/_main_name to create links that are page-aware.
   # In HAML: - nav_tab( 'Homes', url, content: current_tab )
+  # DEPRECATED
   def nav_tab(title, url=title, options={})
     options[:class] = (@page.slug).casecmp(url) == 0 ? 'active' : 'inactive'
     content_tag(:li, link_to(title, url), options )        
